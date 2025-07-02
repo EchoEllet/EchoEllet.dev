6d9baed920a3ab3a056d9c5cbba237adc6b7c987
Ellet <echo.ellet@gmail.com>
2025-07-02 04:05:36 +0300

    chore: configure basePath for GH pages

--- site/src/jsMain/kotlin/dev/echoellet/site/blog/BlogPosts.kt
+++ site/src/jsMain/kotlin/dev/echoellet/site/blog/BlogPosts.kt
@@ -4,6 +4,6 @@ package dev.echoellet.site.blog
 
 object BlogPosts {
     val values = listOf<BlogPostWithRoute>(
-BlogPostWithRoute(blogPost = BlogPost(title = "Flutter Quill: An amazing rich text editor for Quill", description = "My description", tocMin = null ?: BlogPost.DEFAULT_TOC_MIN, tocMax = null ?: BlogPost.DEFAULT_TOC_MAX, author = BlogPost.DEFAULT_AUTHOR, date = "2025-01-02", updated = "2025-01-06"), route = "/blog/2025/temp"), BlogPostWithRoute(blogPost = BlogPost(title = "Compose Multiplatform for Web: An Amazing Framework That Maybe You Shouldn't Use", description = "Compose Multiplatform is a powerful framework for building apps in Kotlin using a shared codebase", tocMin = null ?: BlogPost.DEFAULT_TOC_MIN, tocMax = null ?: BlogPost.DEFAULT_TOC_MAX, author = "David Herman", date = "2024-07-09", updated = null), route = "/blog/2025/c4w")
+
     )
 }
